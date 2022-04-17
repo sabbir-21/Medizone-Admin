df1f80142189443a12da90fe489c7f59b74c1826
Sabbir Ahmed <mdsabbirahmed25@gmail.com>
2022-04-17 22:30:09 +0600

    Added progress dialogue

---
+++ app/src/main/java/com/sabbir/medizoneadmin/HomeActivity.java
@@ -0,0 +1,343 @@
+package com.sabbir.medizoneadmin;
+
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.widget.ImageView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.content.Intent;
+import android.content.ClipData;
+import android.webkit.WebViewClient;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+public class HomeActivity extends Activity {
+	
+	public final int REQ_CD_FP = 101;
+	
+	private Timer _timer = new Timer();
+	
+	private boolean real_time = false;
+	private double real_timer = 0;
+	
+	private LinearLayout linear1;
+	private WebView webview1;
+	private LinearLayout linear2;
+	private ImageView imageview1;
+	
+	private AlertDialog.Builder d;
+	private RequestNetwork r;
+	private RequestNetwork.RequestListener _r_request_listener;
+	private TimerTask t;
+	private Intent fp = new Intent(Intent.ACTION_GET_CONTENT);
+	private ProgressDialog pd;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.home);
+		initialize(_savedInstanceState);
+		
+		if (Build.VERSION.SDK_INT >= 23) {
+			if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+				requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+			} else {
+				initializeLogic();
+			}
+		} else {
+			initializeLogic();
+		}
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		webview1 = findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		linear2 = findViewById(R.id.linear2);
+		imageview1 = findViewById(R.id.imageview1);
+		d = new AlertDialog.Builder(this);
+		r = new RequestNetwork(this);
+		fp.setType("image/*");
+		fp.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				pd = new ProgressDialog(HomeActivity.this);
+				pd.setMax((int)100);
+				pd.setCancelable(false);
+				pd.setCanceledOnTouchOutside(false);
+				pd.setTitle("Please wait while loading");
+				pd.setMessage("Loading...");
+				pd.show();
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				pd.dismiss();
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		_r_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2, HashMap<String, Object> _param3) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				final HashMap<String, Object> _responseHeaders = _param3;
+				imageview1.setVisibility(View.GONE);
+				webview1.setVisibility(View.VISIBLE);
+				real_time = false;
+				r.startRequestNetwork(RequestNetworkController.GET, "https://google.com", "A", _r_request_listener);
+				t = new TimerTask() {
+					@Override
+					public void run() {
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								real_time = true;
+								real_timer = 0;
+							}
+						});
+					}
+				};
+				_timer.schedule(t, (int)(0));
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				real_time = true;
+				r.startRequestNetwork(RequestNetworkController.GET, "https://google.com", "A", _r_request_listener);
+				webview1.setVisibility(View.GONE);
+				imageview1.setVisibility(View.VISIBLE);
+			}
+		};
+	}
+	
+	private void initializeLogic() {
+		webview1.loadUrl("https://medizone.com.bd/wp-admin/");
+		webview1.setWebChromeClient(new WebChromeClient() {
+			// For 3.0+ Devices
+			protected void openFileChooser(ValueCallback uploadMsg, String acceptType) { mUploadMessage = uploadMsg; Intent i = new Intent(Intent.ACTION_GET_CONTENT); i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("image/*"); startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+			}
+			
+			// For Lollipop 5.0+ Devices
+			public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+				if (uploadMessage != null) {
+					uploadMessage.onReceiveValue(null);
+					uploadMessage = null; } uploadMessage = filePathCallback; Intent intent = fileChooserParams.createIntent(); try {
+					startActivityForResult(intent, REQUEST_SELECT_FILE);
+				} catch (ActivityNotFoundException e) {
+					uploadMessage = null; Toast.makeText(getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show(); return false; }
+				return true; }
+			
+			//For Android 4.1 only
+			protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+				mUploadMessage = uploadMsg; Intent intent = new Intent(Intent.ACTION_GET_CONTENT); intent.addCategory(Intent.CATEGORY_OPENABLE); intent.setType("image/*"); startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+			}
+			
+			protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
+				mUploadMessage = uploadMsg; Intent i = new Intent(Intent.ACTION_GET_CONTENT); i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("image/*"); startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+			}
+			
+			
+		});
+		real_time = true;
+		_realtimecheck();
+		imageview1.setVisibility(View.GONE);
+		r.startRequestNetwork(RequestNetworkController.GET, "https://google.com", "A", _r_request_listener);
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			case REQ_CD_FP:
+			if (_resultCode == Activity.RESULT_OK) {
+				ArrayList<String> _filePath = new ArrayList<>();
+				if (_data != null) {
+					if (_data.getClipData() != null) {
+						for (int _index = 0; _index < _data.getClipData().getItemCount(); _index++) {
+							ClipData.Item _item = _data.getClipData().getItemAt(_index);
+							_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _item.getUri()));
+						}
+					}
+					else {
+						_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _data.getData()));
+					}
+				}
+			}
+			break;
+			
+			case REQUEST_SELECT_FILE:
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				if (uploadMessage == null) return; uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(_resultCode, _data)); uploadMessage = null; }
+			break;
+			
+			case FILECHOOSER_RESULTCODE:
+			if (null == mUploadMessage){
+				return; }
+			Uri result = _data == null || _resultCode != RESULT_OK ? null : _data.getData(); mUploadMessage.onReceiveValue(result);
+			mUploadMessage = null;
+			
+			if (true){
+			}
+			else {
+				
+			}
+			break;
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (webview1.canGoBack()) {
+			webview1.goBack();
+		}
+		else {
+			d.setMessage("Do you want to exit from medizone?");
+			d.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface _dialog, int _which) {
+					finish();
+				}
+			});
+			d.setNegativeButton("No", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface _dialog, int _which) {
+					
+				}
+			});
+			d.create().show();
+		}
+	}
+	public void _realtimecheck() {
+		if (real_time) {
+			real_timer++;
+			t = new TimerTask() {
+				@Override
+				public void run() {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							_realtimecheck();
+						}
+					});
+				}
+			};
+			_timer.schedule(t, (int)(0));
+		}
+	}
+	
+	
+	public void _extraforupload() {
+	}
+	
+	private ValueCallback<Uri> mUploadMessage;
+	public ValueCallback<Uri[]> uploadMessage;
+	public static final int REQUEST_SELECT_FILE = 100;
+	
+	private final static int FILECHOOSER_RESULTCODE = 1;
+	{
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
